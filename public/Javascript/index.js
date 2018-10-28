ea9250a4aac5f1778532eacf80d5121415ffbf47
tasiya <siyandamakhalima.1@gmail.com>
2018-10-28 02:29:05 +0200

    changes

--- public/Javascript/index.js
+++ public/Javascript/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     var x = document.querySelector("#demo");
-    let lat = document.querySelector('#lat');
-    let long = document.querySelector('#long');
+    let latt = document.querySelector('#lat');
+    let longg = document.querySelector('#long');
     let address = document.querySelector('.address');
     function getLocation() {
         if (navigator.geolocation) {
@@ -10,20 +10,21 @@ document.addEventListener("DOMContentLoaded", function () {
             x.innerHTML = "Geolocation is not supported by this browser.";
         }
     }
-
+    
     function showPosition(position) {
         console.log(position);
-        
+        latt.innerHTML = position.coords.latitude;
+        longg.innerHTML = position.coords.longitude;
         let lat = position.coords.latitude;
         let long = position.coords.longitude;
-
+        
         var geocoder = new google.maps.Geocoder();
         var latLng = new google.maps.LatLng(lat, long);
 
  if (geocoder) {
     geocoder.geocode({ 'latLng': latLng}, function (results, status) {
        if (status == google.maps.GeocoderStatus.OK) {
-         console.log(results[0].formatted_address); 
+         let addre = results[0].formatted_address; 
          address.innerHTML = results[0].formatted_address;
          $('#address').html('Address:'+results[0].formatted_address);
        }
